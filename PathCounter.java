b0ee9547c61dadd5fc14b80e133fa662bb914937
jrgermain <jrgermain@github.com>
2018-11-28 12:43:15 -0500

    create starter code

--- PathCounter.java
+++ PathCounter.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.io.File;
+
 public class PathCounter {
+	public static void main(String[] args) {
+		// Read input file
+		try {
+            File input = new File("simple0.input");
+        } catch (NullPointerException e) {
+            System.err.println("Error: file" + input.getName() + "not found");
+            System.exit(1);
+}
+		// Parse input file
+		int numVertices;
+		int numEdges;
+		ArrayList<Edge> edges = new ArrayList<Edge>();
 
-  public static void main (String[] args) {
+		//TODO: make first line assign numVertices and numEdges
+		//TODO: for each additional line, make new edge and add to ArrayList
 
-  }
+		//TODO: calculations 
+	}
 
+	private class Edge {
+		public int a,b;
+		public Edge(int a, int b) {
+			this.a = a;
+			this.b = b;
+		}
+	}
 }
